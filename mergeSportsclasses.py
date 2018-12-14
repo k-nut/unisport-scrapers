c3ca867d553e2d77ce70a8f67e9b9ca167984cbf
Knut Hühne <knut@k-nut.eu>
2018-12-14 11:00:33 +0100

    do not crash if result json files are empty

--- mergeSportsclasses.py
+++ mergeSportsclasses.py
@@ -1,20 +1,29 @@
 import json
 import os
-import os
+
 JSON_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)), "data")
 
 print("Merging data in {}".format(JSON_PATH))
 
-with open(os.path.join(JSON_PATH, 'hu.json')) as infile:
-    hu = json.loads(infile.read())
-with open(os.path.join(JSON_PATH, 'fu.json')) as infile:
-    fu = json.loads(infile.read()) 
-with open(os.path.join(JSON_PATH, 'htw.json')) as infile:
-    htw = json.loads(infile.read())
-with open(os.path.join(JSON_PATH, 'beuth.json')) as infile:
-    beuth = json.loads(infile.read())        
-with open(os.path.join(JSON_PATH, 'tu.json')) as infile:
-    tu = json.loads(infile.read())      
+
+def json_or_empty(infile):
+    try:
+        return json.load(infile)
+    except ValueError:
+        return []
+
+def load_file(filename):
+    with open(os.path.join(JSON_PATH, filename)) as infile:
+        result = json_or_empty(infile)
+        if len(result) == 0:
+            print('Got an empty list for ', filename)
+        return result
+
+hu = load_file('hu.json')
+fu = load_file('fu.json')
+htw = load_file('htw.json')
+beuth = load_file('beuth.json')
+tu = load_file('tu.json')
 
 with open(os.path.join(JSON_PATH, 'alle.json'), 'w') as outfile:
     outfile.write(json.dumps(hu[1:] + fu[1:] + beuth[1:] + htw[1:] + tu))
