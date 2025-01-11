d6ce14fe2f758bcfe2ca4055c7ce394d595b2c59
Knut Hühne <knut@k-nut.eu>
2025-01-11 07:43:39 +0100

    Update domain for beuth to new university name

--- sportsClasses/spiders/beuth.py
+++ sportsClasses/spiders/beuth.py
@@ -3,8 +3,8 @@ from .sports_spider import SportsSpider
 
 class BeuthSpider(SportsSpider):
     name = "beuth"
-    allowed_domains = ["zeh02.beuth-hochschule.de"]
+    allowed_domains = ["zeh02.bht-berlin.de"]
     start_urls = (
-        'https://zeh02.beuth-hochschule.de/angebote/aktueller_zeitraum/index.html',
+        'https://zeh02.bht-berlin.de/angebote/aktueller_zeitraum/index.html',
     )
 
